3ec82ef8ae0acfca3b5116e99c1a429318be880c
Sean Zhang <sean.zhang@d1m.cn>
2018-02-28 21:01:33 +0800

    Commit Test

---
+++ com.sean.java/com.sean.java.design-pattern/src/main/java/com/sean/java/design/pattern/App.java
@@ -0,0 +1,13 @@
+package com.sean.java.design.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
